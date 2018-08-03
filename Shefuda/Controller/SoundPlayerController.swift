70318301638236d2cc6e39fcda01f10ffe2de966
Shawon Ashraf <shawon13@live.com>
2018-08-03 15:37:53 +0600

    play and getSoundFileURL method

--- Shefuda/Controller/SoundPlayerController.swift
+++ Shefuda/Controller/SoundPlayerController.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import SwiftySound
+
+
+class SoundPlayerController {
+    
+    // MARK: This class is a singleton
+    
+    // plays soundclip by title
+    func play(soundTitle title: String) {
+        // fetch sound URL
+        if let soundURL = getSoundFileURL(title: title) {
+            Sound.play(url: soundURL)
+        } else {
+            print("File doesn't exist")
+        }
+        
+    }
+    
+    // gets soundfile url by title
+    func getSoundFileURL(title: String) -> URL? {
+        // fetch from static array
+        let index = SoundClipData.soundClips.index(of: title)!
+        let soundResource = SoundClipData.soundClips[index]
+        
+        let file = Bundle.main.path(forResource: soundResource, ofType: SoundClipData.fileExtension)
+        
+        if let fileURL = file {
+            print("playing!")
+            let url = URL(fileURLWithPath: fileURL)
+            return url
+        } else {
+            print("File doesn't exist")
+            return nil
+        }
+    }
+    
+}
