d20b27212ab32ebb20eb3f3dc79c719ca4bfd366
Shawon Ashraf <shawon13@live.com>
2018-09-28 01:30:35 +0600

    changed player module to AVAudioPlayer

--- Shefuda/Controller/SoundPlayerController.swift
+++ Shefuda/Controller/SoundPlayerController.swift
@@ -7,37 +7,65 @@
 //
 
 import Foundation
+import AVFoundation
 
 final class SoundPlayerController {
     
     // MARK: This class is a singleton
     static let soundPlayer: SoundPlayerController = SoundPlayerController()
     
+    // MARK: audioPlayer init
+    private var audioPlayer = AVAudioPlayer()
+    
     // MARK: Helper methods
     // plays soundclip by title
     func play(soundTitle title: String) {
-       
+        stop()
+        if let soundFileURL = getSoundFileURL(title: title) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundFileURL)
+                audioPlayer.prepareToPlay()
+                
+            
+                // init audio player session
+                let session = AVAudioSession.sharedInstance()
+                do {
+                    try session.setCategory(.playback, mode: .spokenAudio, options: .defaultToSpeaker)
+                } catch let error {
+                    print(error)
+                }
+            } catch let error {
+                print(error)
+            }
+            
+            // now play
+            audioPlayer.play()
+        } else {
+            print("File wasn't found!")
+        }
     }
     
     // gets soundfile url by title
-//    func getSoundFileURL(title: String) -> URL? {
-//        let soundResource = SoundClipData.soundClips[title]
-//
-//        let file = Bundle.main.path(forResource: soundResource, ofType: SoundClipData.fileExtension)
-//
-//        if let fileURL = file {
+    func getSoundFileURL(title: String) -> URL? {
+        let soundResource = SoundClipData.soundClips[title]
+
+        let file = Bundle.main.path(forResource: soundResource, ofType: SoundClipData.fileExtension)
+
+        if let fileURL = file {
 //            print("playing!")
-//            let url = URL(fileURLWithPath: fileURL)
-//            return url
-//        } else {
-//            print("File doesn't exist")
-//            return nil
-//        }
-//    }
+            let url = URL(fileURLWithPath: fileURL)
+            return url
+        } else {
+            print("File doesn't exist!")
+            return nil
+        }
+    }
     
     // stop playing
-//    func stop() {
-//        Sound.stopAll()
-//    }
+    func stop() {
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+    }
     
 }
